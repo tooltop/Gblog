57ada5ec0c8e36d40e8c1e7f4757298d8d8be75d
jax <169969144+tooltop@users.noreply.github.com>
2024-06-24 23:38:38 +0800

    Update and rename Gmeek.py to Gblog.py

--- Gblog.py
+++ Gblog.py
@@ -33,7 +33,7 @@ IconBase={
     "sync":"M1.705 8.005a.75.75 0 0 1 .834.656 5.5 5.5 0 0 0 9.592 2.97l-1.204-1.204a.25.25 0 0 1 .177-.427h3.646a.25.25 0 0 1 .25.25v3.646a.25.25 0 0 1-.427.177l-1.38-1.38A7.002 7.002 0 0 1 1.05 8.84a.75.75 0 0 1 .656-.834ZM8 2.5a5.487 5.487 0 0 0-4.131 1.869l1.204 1.204A.25.25 0 0 1 4.896 6H1.25A.25.25 0 0 1 1 5.75V2.104a.25.25 0 0 1 .427-.177l1.38 1.38A7.002 7.002 0 0 1 14.95 7.16a.75.75 0 0 1-1.49.178A5.5 5.5 0 0 0 8 2.5Z"
 }
 ######################################################################################
-class GMEEK():
+class Gblog():
     def __init__(self,options):
         self.options=options
         
@@ -159,8 +159,8 @@ class GMEEK():
                         f'color: var(--fgColor-{style},var(--color-{style}-fg));}}</style>'
                     )
 
-        if '<code class="notranslate">Gmeek-html' in post_body:
-            post_body = re.sub(r'<code class="notranslate">Gmeek-html(.*?)</code>', lambda match: html.unescape(match.group(1)), post_body, flags=re.DOTALL)
+        if '<code class="notranslate">Gblog-html' in post_body:
+            post_body = re.sub(r'<code class="notranslate">Gblog-html(.*?)</code>', lambda match: html.unescape(match.group(1)), post_body, flags=re.DOTALL)
 
         postBase["postTitle"]=issue["postTitle"]
         postBase["postUrl"]=self.blogBase["homeUrl"]+"/"+issue["postUrl"]
@@ -421,7 +421,7 @@ parser.add_argument("repo_name", help="repo_name")
 parser.add_argument("--issue_number", help="issue_number", default=0, required=False)
 options = parser.parse_args()
 
-blog=GMEEK(options)
+blog=Gblog(options)
 
 if not os.path.exists("blogBase.json"):
     print("blogBase is not exists, runAll")
@@ -484,7 +484,7 @@ if os.environ.get('GITHUB_EVENT_NAME')!='schedule':
     readme=readme+"### :speech_balloon: %d \r\n" % commentNumSum
     readme=readme+"### :hibiscus: %d \r\n" % wordCount
     readme=readme+"### :alarm_clock: %s \r\n" % datetime.datetime.now(blog.TZ).strftime('%Y-%m-%d %H:%M:%S')
-    readme=readme+"### Powered by :heart: [Gmeek](https://github.com/Meekdai/Gmeek)\r\n"
+    readme=readme+"### Powered by :heart: [Gblog](https://github.com/tooltop/Gblog)\r\n"
     readmeFile=open(workspace_path+"/README.md","w")
     readmeFile.write(readme)
     readmeFile.close()
